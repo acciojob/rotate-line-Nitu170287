d5f2149afb5e0bc185f38030bb0d266bd1bff21e
Nitu Dubey <108494260+Nitu170287@users.noreply.github.com>
2023-06-14 15:51:24 -0400

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 //your JS code here. If required.
  const Line = document.getElementById('line');
    Line.style.transform = 'matrix(1, 0, 0, 1, 100, 100)';
+setTimeout(()=>{
+	resolve()
+},1000)
+Line.style.transform = 'matrix(-0.5, 0.866025, -0.866025, -0.5, 0, 0)';
