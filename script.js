dd1609b66e29c39bf87b398750d94a7b7b083d83
Nitu Dubey <108494260+Nitu170287@users.noreply.github.com>
2023-06-14 14:22:30 -0400

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
 //your JS code here. If required.
-
-let line = document.getElementById("line")
-line.style.transform = "rotate(180deg)"
+ const line = document.getElementById('line');
+    line.style.transform = 'rotate(90deg)';
