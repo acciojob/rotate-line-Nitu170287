ae48945f6fc39975a32660d48dc1613ca124980b
Nitu Dubey <108494260+Nitu170287@users.noreply.github.com>
2023-06-14 16:24:59 -0400

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 //your JS code here. If required.
+
  const Line = document.getElementById('line');
-   Line.style.transform = 'matrix(1, 0, 0, 1, 100, 100)';
-setInterval(()=>{
-	resolve()
-},500)
-Line.style.transform = 'matrix(-0.5, 0.866025, -0.866025, -0.5, 0, 0)';
+   Line.style.transform = 'matrix(1, 2, 1, 1, 200, 100)';
+setInterval(function() {
+	//console.log('matrix(-0.5,'+ Math.floor(Math.random() * 100) + 1+', -'+Math.floor(Math.random() * 100) + 1+', -0.5, 0, 0)')
+  Line.style.transform = 'matrix(1,'+ Math.floor(Math.random() * 10) + 1+', 1, 1, 200,100)';
+}, 500);
+
